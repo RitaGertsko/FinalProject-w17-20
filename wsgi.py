09a3bffaeb9b7a2855f88aed9095af78dafb674c
Rita Margalit Gertskovich <rita1234rita@gmail.com>
2022-07-06 15:01:43 +0300

    encapsulate

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,7 @@
 from shoppingApp import create_app, db
 
-
 app = create_app()
 db.create_all(app=app)
 
-app.run(debug=False, port=5020)
+if __name__ == '__main__':
+    app.run(debug=False, port=5020)
